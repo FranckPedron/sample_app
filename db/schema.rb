6b7303a29f7833570240228f4a7a19e16356c8b3
Franck P <franckyp1+rapports_git@gmail.com>
2016-02-25 13:12:54 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160218171013) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "nom"
     t.string   "email"
@@ -20,6 +23,6 @@ ActiveRecord::Schema.define(version: 20160218171013) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
